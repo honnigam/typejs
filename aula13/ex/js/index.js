6b3da94e86b36c8f56cdf76d7946a5c3f5f5134d
Lucas Barcelos <lucasvm87@gmail.com>
2023-03-15 20:20:46 -0300

    update lesson exercices

---
+++ aula13/ex/js/index.js
@@ -0,0 +1,10 @@
+const nome = prompt('Digite seu nome completo: ')
+document.body.innerHTML += `O seu nome é: ${nome} <br/>`
+document.body.innerHTML += `Seu nome tem: ${nome.length} letras <br/>`
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome.charAt(1)} <br/>`
+document.body.innerHTML += `Qual o primeiro índice da letra 'a' no seu nome? ${nome.indexOf('a')}<br/>`
+document.body.innerHTML += `qual o ultimo indice da letra no 'a' seu nome? ${nome.lastIndexOf('a')} <br/>`
+document.body.innerHTML += `as ultimas 3 letras do seu nome são: ${nome.slice(-3, nome.length)}<br/>`
+document.body.innerHTML += `as palavras do seu nome são: ${nome.split(' ')}<br/>`
+document.body.innerHTML += `seu nome com letras maiusculas: ${nome.toUpperCase()} <br/>`
+document.body.innerHTML += `seu nome com letras minusculas: ${nome.toLowerCase()}<br/>`
