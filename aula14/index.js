ac5c65e9e4ba8028f8d99bece288f377142de0d6
Lucas Barcelos <lucasvm87@gmail.com>
2023-03-15 20:45:11 -0300

    creating functions for round numbers

--- aula14/index.js
+++ aula14/index.js
@@ -6,5 +6,17 @@ let num2 = 2.5;
 // console.log(num1.toString(2))
 // console.log(num1.toFixed(2)) //arendando com o toFixed - numeros
 // console.log(Number.isInteger(num1)) //retorna um valor booleano - validando a solicitação Inteira
-let temp = num1 * '5'
-console.log(Number.isNaN(temp)) //se a conta passa ele retorna false porque o parametro é para validação do Number
+// let temp = num1 * '5'
+// console.log(Number.isNaN(temp)) //se a conta passa ele retorna false porque o parametro é para validação do Number
+
+let num3 = 0.7
+let num4 = 0.1
+num3 += num4; // 0.8 //num3 = num 3 + num4 
+num3 += num4; // 0.9 
+num3 += num4; // 1.0 
+num3 += num4; // 1.1 
+
+// console.log(num3.toFixed(2)) //não resolve o problema de arrendondamento
+num3 = Number(num3.toFixed(2))
+console.log(Number.isInteger(num3)) //checando se é um numero inteiro 
+console.log(num3)
