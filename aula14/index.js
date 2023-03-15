d54f5695d863103cac899859d30a9f91a76668d7
Lucas Barcelos <lucasvm87@gmail.com>
2023-03-15 20:33:53 -0300

    update structures

--- aula14/index.js
+++ aula14/index.js
@@ -1,5 +1,10 @@
-let num1 = 1;
+let num1 = 10.1351515125;
 let num2 = 2.5;
 
-console.log(num1.toString() + num2.toString()) //convertendo o valor temporariamente para string na função
-console.log(num1 * num2)
+// console.log(num1.toString() + num2.toString()) //convertendo o valor temporariamente para string na função
+// num1 = num1.toString()
+// console.log(num1.toString(2))
+// console.log(num1.toFixed(2)) //arendando com o toFixed - numeros
+// console.log(Number.isInteger(num1)) //retorna um valor booleano - validando a solicitação Inteira
+let temp = num1 * '5'
+console.log(Number.isNaN(temp)) //se a conta passa ele retorna false porque o parametro é para validação do Number
