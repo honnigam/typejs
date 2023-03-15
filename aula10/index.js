dddb78fb9b2837b57ce5f6f36e8dd1a00c9110f9
Lucas B Arce <ARCELB@sa.ad.whirlpool.com>
2023-03-15 15:50:49 -0300

    update content jsts

---
+++ aula10/index.js
@@ -0,0 +1,36 @@
+/* 
+* aritmétricos
+* '+' adição / concatenação
+* '-' subtração
+* '*' multiplicação
+* '/' divisão 
+* '**' - elevação
+* '%' - resto da divisão  
+* ordem de prioridade -> (), **, * / %, + -
+*/ 
+
+// const num1 = 5
+// const num2 = 2
+// const num3 = 10
+// console.log((num1 + num2) * num3)
+
+// let contador = 10 
+// contador++; //incrementa depois que executa a conta
+// console.log(contador)
+// --contador; //primeiro executa a incrementação ou decrementação
+// console.log(contador)
+
+
+//operador de atribuição
+let contador = 2
+contador **= 1
+console.log(contador)
+
+//erro de calculo - NaN not a number
+//parseInt() - converte pra inteiro
+//parseFloat() - converte pra flutuante
+//Number() - define como um numero 
+
+const num1 = 10
+const num2 = Number('5.9')
+console.log(num1 + num2)
--- aula9/index.js
+++ aula9/index.js
@@ -1,4 +1,4 @@
-//dados primitivos
+//dados primitivos -> string, number, indefined, null, boolean, symbol
 
 const nome = 'Lucas'
 const nome1 = "Lucas Barcelos"
@@ -8,3 +8,13 @@ const num2 = 10.52
 let nomeAluno // undefined -> não aponta pra local nenhum na memória
 const sobrenomeAluno = null // nulo -> não aponta pr anenhum local na memória quando NULO
 const aprovado = true //boolean -> false / true
+
+//ambos possuem o mesmo valor -> b copiou o valor de a
+//valores em constante, quando forem necessitados alterar executar 'let'
+const a = [1,2]
+const b = a
+console.log (a,b)
+
+b.push(3)
+console.log (a,b)
+
