3d40e66b993ba7c3e4ba78d66238d28733e33fb2
Lucas B Arce <ARCELB@sa.ad.whirlpool.com>
2023-03-16 14:06:11 -0300

    solved the exercises

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -4,5 +4,11 @@ const numeroTitulo = document.getElementById('numero-titulo'); //pega o elemento
 const texto = document.getElementById('texto'); //pega o elemento da DIV relacionando com o ID e salva na variavel atribuida 
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p> Seu numero + 2 é: ${numero + 2}.</p>`;
+texto.innerHTML += `<p> Seu numero + 2 é: ${numero + 2}.</p>`;
+texto.innerHTML += `<p> Raiz quadrada de ${numero} é: ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p> ${numero} é inteiro? ${Number.isInteger(numero)}.</p>`;
+texto.innerHTML += `<p> ${numero}	é NaN? ${Number.isNaN(numero)}</p>`
+texto.innerHTML += `<p> ${numero} arredondado para baixo é: ${Math.floor(numero)} </p>`
+texto.innerHTML += `<p> ${numero} arredondado para cima é: ${Math.ceil(numero)} </p>`
+texto.innerHTML += `<p> este numero com duas casas decimais é: ${numero.toFixed(2)}</p>`
 
