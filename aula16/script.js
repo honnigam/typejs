b263c6d4ec4838638e822f9104e7a2cc0dd98b2a
Lucas B Arce <ARCELB@sa.ad.whirlpool.com>
2023-03-16 16:09:19 -0300

    update functions

--- aula16/script.js
+++ aula16/script.js
@@ -2,11 +2,11 @@
 // const nome = 'Lucas Barcelos'
 // console.log(nome[2]) //consultando uma indexação
 
-const alunos = ['Luiz', 'Maria', 'João'];
-alunos[alunos.length] = 'Lucas'
-alunos[alunos.length] = 'Fábio' //formas de adicionar mais um array 
-console.log(alunos.length) //contando a quantidade de elementos
-console.log(alunos)
+// const alunos = ['Luiz', 'Maria', 'João'];
+// alunos[alunos.length] = 'Lucas'
+// alunos[alunos.length] = 'Fábio' //formas de adicionar mais um array 
+// console.log(alunos.length) //contando a quantidade de elementos
+// console.log(alunos)
 
 // console.log(alunos)
 // console.log(alunos[0]) //exibe o elemento
@@ -15,3 +15,14 @@ console.log(alunos)
 // alunos [0] = 'Eduardo' //editando o índice 0
 // alunos [3] = 'Lucas' //adicionando um novo indice - forma complicada 
 // console.log(alunos)
+
+const alunos = ['Luiz', 'Maria', 'João'];
+// alunos.push('Lucas')
+// alunos.push('Mario') //com a função push ele insere diretamente no array
+// alunos.unshift('Lucas') //adiciona no começo do array
+// alunos.unshift('Fábio') //adiciona no começo do array
+// alunos.unshift('William') //adiciona no começo do array sendo o mais embaixo o primeiro da lista. Para mudar a sequencia só adicionar numa ordem inversa 
+alunos.pop(); //tira o ultimo elemento do array podendo também salvar em uma var
+const removido = alunos.pop()
+console.log(removido);
+console.log(alunos);
