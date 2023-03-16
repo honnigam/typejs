eee2c4503ee4817d6ebae955bb9e82cc34486800
Lucas B Arce <ARCELB@sa.ad.whirlpool.com>
2023-03-16 14:56:36 -0300

    add more arrays in one fixed array

--- aula16/script.js
+++ aula16/script.js
@@ -1,3 +1,17 @@
-//arrays
+//arrays - indexão por elemento, não caracteres
+// const nome = 'Lucas Barcelos'
+// console.log(nome[2]) //consultando uma indexação
 
 const alunos = ['Luiz', 'Maria', 'João'];
+alunos[alunos.length] = 'Lucas'
+alunos[alunos.length] = 'Fábio' //formas de adicionar mais um array 
+console.log(alunos.length) //contando a quantidade de elementos
+console.log(alunos)
+
+// console.log(alunos)
+// console.log(alunos[0]) //exibe o elemento
+// console.log(alunos[2]) //exibe o elemento
+
+// alunos [0] = 'Eduardo' //editando o índice 0
+// alunos [3] = 'Lucas' //adicionando um novo indice - forma complicada 
+// console.log(alunos)
