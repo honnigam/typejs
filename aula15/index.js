8bb2bea8ddc10f36e4e5daab83262709ee604848
Lucas Barcelos <lucasvm87@gmail.com>
2023-03-15 21:28:08 -0300

    finish the lesson 15

--- aula15/index.js
+++ aula15/index.js
@@ -8,7 +8,16 @@ let num1 = 9.51465
 // console.log(Math.max(1,5,654,1984984,1541984,151,15,1412,6516,1))
 // console.log(Math.min(11,5,654,1984984,1541984,151,15,1412,6516,1))
 
-const random2 = Math.random() * (10 - 5) + 5 //multiplica o padrão pelo estabelecido e cria um limite entre max 10 e min 5 e no final adicionando 5
-console.log(random2) 
-const random = Math.round(Math.random() * (10-5) + 5); //faz a mesma coisa que a linha 11 porém arredonda no final
-console.log(random)
+// const random2 = Math.random() * (10 - 5) + 5 //multiplica o padrão pelo estabelecido e cria um limite entre max 10 e min 5 e no final adicionando 5
+// console.log(random2) 
+// const random = Math.round(Math.random() * (10-5) + 5); //faz a mesma coisa que a linha 11 porém arredonda no final
+// console.log(random)
+
+console.log(Math.pow(2, 10)) //elevando x a y 
+// ou 
+console.log (2 ** 10)
+
+let num3 = 25
+console.log(num3 ** 0.5) //obtendo a raiz quadrada de um numero de forma direta
+
+console.log(!!(100 / 0)); //isso retorna TRUE em JS, pois retorna o numero mais perto de infinito - porém em operação matematica isso não existe
