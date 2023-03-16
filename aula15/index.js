c30003b5f3659e2525403f5e98ec962a4ffb721b
Lucas Barcelos <lucasvm87@gmail.com>
2023-03-15 21:21:10 -0300

    adding mathematical operators in the math library

--- aula15/index.js
+++ aula15/index.js
@@ -1,1 +1,14 @@
 //method math
+
+let num1 = 9.51465
+// let num2 = Math.floor(num1) // função para arredondar para baixo
+// let num2 = Math.ceil(num1) // arredonda para cima
+// let num2 = Math.round(num1) //arredonda para o mais proximo
+// console.log(num2)
+// console.log(Math.max(1,5,654,1984984,1541984,151,15,1412,6516,1))
+// console.log(Math.min(11,5,654,1984984,1541984,151,15,1412,6516,1))
+
+const random2 = Math.random() * (10 - 5) + 5 //multiplica o padrão pelo estabelecido e cria um limite entre max 10 e min 5 e no final adicionando 5
+console.log(random2) 
+const random = Math.round(Math.random() * (10-5) + 5); //faz a mesma coisa que a linha 11 porém arredonda no final
+console.log(random)
