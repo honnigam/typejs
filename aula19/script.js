99579009c38c73de254f139b8a17327d3f1c23b9
Lucas Barcelos <lucasvm87@gmail.com>
2023-03-20 15:05:58 -0300

    update lesson 19

--- aula19/script.js
+++ aula19/script.js
@@ -20,18 +20,28 @@ Referencia (mutável) - arrays, object e functions
 VALORES PASSADOS POR REFERENCIA  
 */
 
-let a = [1,2,3] //dado passado por referencia - quando alterado, altera em tudo
-let b = a 
-console.log(a,b)
+// let a = [1,2,3]; //dado passado por referencia - quando alterado, altera em tudo
+// let b = [...a]; // valor de A copiado em B, nesse caso valor de B é totalmente independente - sendo um spread [...]
+// let c = b
+// console.log(a,b)
 
-a.push(4)
-console.log(a) //se ler B, também foi afetado pois altera diretamente o valor na memória
-console.log (a,b)
+// a.push(4)
+// console.log(a) //se ler B, também foi afetado pois altera diretamente o valor na memória
+// console.log (a,b)
 
-b.pop();
-console.log(a,b) //quando retirado, retira em tudo, pois está apontando diretamente na memória
+// b.pop();
+// console.log(a,b) //quando retirado, retira em tudo, pois está apontando diretamente na memória
 
-let c = b //C apontando pra B que aponta pra A 
-console.log(c)
-a.push('Luiz') //adicionando uma string em A
-console.log(c) //array sendo afetado em A e exibindo em C
+// console.log(c)
+// a.push('Luiz') //adicionando uma string em A
+// console.log(c) //array sendo afetado em A e exibindo em C
+
+const a = {
+  nome: 'Lucas',
+  sobrenome: 'Barcelos',
+};
+
+const b = {...a}; //b aponta para o mesmo local que A, executando o spread ele se torna independente 
+a.nome = 'Matheus'
+console.log(a);
+console.log(b);
